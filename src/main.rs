c90ee20d445f538d38be6a0dfe92cf0455f1ec7e
serayuzgur <serayuzgur@gmail.com>
2016-10-27 13:52:38 +0300

    Changes made according to the code review from #rust_beginners. round folder removed. Round.rs renamed to round.rs for windows compilation issues. "while" with counter changed to "for" without any variable.

--- src/main.rs
+++ src/main.rs
@@ -7,29 +7,24 @@ fn main() {
     let mut changed_wins = 0;
     let mut not_changed_wins = 0;
     let limit = 1000000;
-    println!("Creating {} Rounds...", limit);
 
-    // I only need to loop 1000000 times. Couldn't find a way without defining counter.
-    let mut counter = 0;
-    while counter < limit {
+    println!("Creating {} Rounds...", limit);
+    for _ in 0..limit {
         let mut round = round::Round::new();
         round.reveal_door();
         if round.is_victory() {
             not_changed_wins += 1;
         }
-        counter = counter + 1;
     }
 
     println!("Creating {} Rounds...", limit);
-    let mut counter = 0;
-    while counter < limit {
+    for _ in 0..limit {
         let mut round = round::Round::new();
         round.reveal_door();
         round.change_selection();
         if round.is_victory() {
             changed_wins += 1;
         }
-        counter = counter + 1;
     }
 
     println!(" Changed:{} Not Changed:{} Rate:{}",
