7c8130f798ee1401ed1a0ef14b313ee325fd55a0
serayuzgur <serayuzgur@gmail.com>
2016-10-28 10:06:40 +0300

    tests

--- src/round.rs
+++ src/round.rs
@@ -18,7 +18,7 @@ impl Round {
         Round {
             doors: doors,
             selection: rand::random::<usize>() % 3,
-            revealed: 0,
+            revealed: 3, // invalid number out of index
         }
     }
     /// Select a random door from doors.
@@ -26,6 +26,7 @@ impl Round {
         for i in 0..3 {
             if i != self.selection && self.doors[i as usize] == 0 {
                 self.revealed = i;
+                break;
             }
         }
     }
@@ -33,10 +34,38 @@ impl Round {
         for i in 0..3 {
             if i != self.selection && i != self.revealed {
                 self.selection = i;
+                break;
             }
         }
     }
     pub fn is_victory(&self) -> bool {
         self.doors[self.selection] == 1
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn reveal_door() {
+        let mut round = Round::new();
+        round.reveal_door();
+
+        assert!(round.revealed >= 0 as usize && round.revealed < 3 as usize,
+                "reveal_door must assign a value to round.revealed");
+    }
+
+    #[test]
+    fn change_selection() {
+        let mut round = Round::new();
+        let first_selection = round.selection;
+        round.reveal_door();
+        round.change_selection();
+        let is_changed = round.selection != first_selection && round.selection != round.revealed;
+        assert!(is_changed,
+                "change_selection must change round.selection before:{} after:{}",
+                first_selection,
+                round.selection);
     }
 }
